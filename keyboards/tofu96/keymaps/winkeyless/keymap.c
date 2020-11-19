4b7d2005189c85eea807e9f599ef2bdc298d884f
PINPAL <18197217+PINPAL@users.noreply.github.com>
2020-11-19 17:01:30 +0000

    Vim is a lifestyle

--- keyboards/tofu96/keymaps/winkeyless/keymap.c
+++ keyboards/tofu96/keymaps/winkeyless/keymap.c
@@ -34,27 +34,18 @@
 #define ________ 	KC_TRNS 	// Transparent Key
 
 // Macro Shorthand
-#define M_RUN	LGUI(KC_R)		// Windows + R	Open Run
-#define M_LCK	LGUI(KC_L)		// Windows + L	Lock Screen
-#define M_AC	LGUI(KC_A)		// Windows + A	Open Action Center
-#define M_KL	LGUI(KC_SPC)	// Windows + Space Switch Keyboard Layout
-
 #define M_LEFT	SWIN(KC_LEFT)	// Shift + Win + ◄	Move Window Left
 #define M_RGHT	SWIN(KC_RGHT)	// Shift + Win + ►	Move Window Right
-#define M_WT	LGUI(KC_UP)		// Windows + ▲	Split Window Top   
-#define M_WB	LGUI(KC_DOWN)	// Windows + ▼	Split Window Down
-#define M_WL	LGUI(KC_LEFT)	// Windows + ◄	Split Window Left
-#define M_WR	LGUI(KC_RGHT)	// Windows + ►	Split Window Right
 
 // Right Alt & Ctrl act as normal when tapped but switch to layer 1 while held
 #define FN_ALT	LT(1,KC_RALT)
 #define FN_CTRL LT(1,KC_RCTL)
 
 // Force NUMLOCK all the time
-void led_set_keymap(uint8_t usb_led) { 		// Called to check LED lights status upon connection
+void led_set_keymap(uint8_t usb_led) { 			// Called to check LED lights status upon connection
   if (!(usb_led & (1<<USB_LED_NUM_LOCK))) { // Check if NUMLOCK enabled
-    register_code(KC_NUMLOCK);				// Simulate NUMLOCK key down stroke
-    unregister_code(KC_NUMLOCK); 			// Simulate NUMLOCK key up stroke
+    register_code(KC_NUMLOCK);							// Simulate NUMLOCK key down stroke
+    unregister_code(KC_NUMLOCK); 						// Simulate NUMLOCK key up stroke
   }
 }
 
@@ -64,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// Main Layer
 	[0] = LAYOUT_default(
 	//┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐
-	//│ Esc    │ F1     │ F2     │ F3     │ F4     │ F5     │ F6     │ F7     │ F8     │ F9     │ F10    │ F11    │ F12    │ Delete │ Home   │ Play   │ Vol +  │ Vol -  │ F24    │
-			KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_DEL , KC_HOME, KC_MPLY, KC_VOLD, KC_VOLU, KC_F24 ,\
+	//│ Esc    │ F1     │ F2     │ F3     │ F4     │ F5     │ F6     │ F7     │ F8     │ F9     │ F10    │ F11    │ F12    │ Delete │ Home   │ End    │ Vol +  │ Vol -  │ F24    │
+			KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_DEL , KC_HOME, KC_END, KC_VOLD, KC_VOLU, KC_F24 ,\
 	//├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┴────────┼────────┼────────┼────────┼────────┤
 	//│ ~      │ 1      │ 2      │ 3      │ 4      │ 5      │ 6      │ 7      │ 8      │ 9      │ 0      │ -      │ +      │ Backspace       │ ,      │ /      │ *      │ -      │
 			KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC         , KC_COMM, KC_PSLS, KC_PAST, KC_PMNS,\
@@ -79,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//│ Shift            │ Z      │ X      │ C      │ V      │ B      │ N      │ M      │ <      │ >      │ ?      │ Shift          │ ▲      │ 1      │ 2      │ 3      │        │	 
 			KC_LSFT          , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT        , KC_UP  , KC_P1  , KC_P2  , KC_P3  , KC_ENT ,\
 	//├─────────┬────────┴┬───────┴─┬──────┴────────┴────────┴────────┴────────┴────────┴────────┼────────┴───┬────┴───────┬────────┼────────┼────────┼────────┼────────┤ Enter  │
-	//│ Ctrl    │ FN → 1  │ Alt     │ Space                                                      │ Alt        │ CTRL       │ ◄      │ ▼      │ ►      │ 0      │ •      │        │
+	//│ Ctrl    │ FN → 1  │ Alt     │ Space                                                      │ Alt        │ Ctrl       │ ◄      │ ▼      │ ►      │ 0      │ •      │        │
 			KC_LCTL , MO(1)   , KC_LALT , KC_SPC                                                     , FN_ALT     , FN_CTRL    , KC_LEFT, KC_DOWN, KC_RGHT, KC_P0  , KC_PDOT            
 	//└─────────┴─────────┴─────────┴────────────────────────────────────────────────────────────┴────────────┴────────────┴────────┴────────┴────────┴────────┴────────┴────────┘
 	),
@@ -87,23 +78,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// Main Function Layer
 	[1] = LAYOUT_default(
 	//┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐
-	//│ Layout │        │        │        │        │        │        │        │        │        │        │        │ Insert │ PrntScr│ End    │ Mute   │ Skip ♫ │ Next ♫ │ F23    │
-	    M_KL   ,________,________,________,________,________,________,________,________,________,________,________, KC_INS , KC_PSCR, KC_END , KC_MUTE, KC_MPRV, KC_MNXT, KC_F23 ,\
+	//│        │        │        │        │        │        │        │        │        │        │        │        │ Insert │ PrntScr│ Mute ♫ │ Play ♫ │ Skip ♫ │ Next ♫ │ F23    │
+	   ________,________,________,________,________,________,________,________,________,________,________,________, KC_INS , KC_PSCR, KC_MUTE, KC_MPLY, KC_MPRV, KC_MNXT, KC_F23  ,\
 	//├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┴────────┼────────┼────────┼────────┼────────┤
 	//│        │        │        │        │        │        │        │        │        │        │        │        │        │                 │ NumLock│        │        │        │
 	   ________,________,________,________,________,________,________,________,________,________,________,________,________,________         , KC_NLCK,________,________,________,\
 	//├────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬─────────────┼────────┼────────┼────────┤────────┤
-	//│            │        │ ▲      │        │ Run    │        │        │        │        │        │        │        │        │             │        │        │        │        │
-	   ________     ,________, KC_UP  ,________, M_RUN  ,________,________,________,________,________,________,________,________,________     ,________, M_WT   ,________,________,\
+	//│            │        │        │        │        │        │        │        │        │        │        │        │        │             │        │        │        │        │
+	   ________    ,________,________,________,________,________,________,________,________,________,________,________,________,________     ,________,________,________,________,\
 	//├────────────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─────────────┼────────┼────────┼────────┤        │
-	//│              │ ◄      │ ▼      │ ►      │        │        │        │        │        │ Lock   │        │        │                    │        │        │        │        │
-	   KC_LGUI      , KC_LEFT, KC_DOWN, KC_RGHT,________,________,________,________,________, M_LCK  ,________,________,________            , M_WL   ,________, M_WR   ,         \
+	//│              │        │        │        │        │        │ ◄      │ ▼      │ ▲      │ ►      │        │        │                    │        │        │        │        │
+	   ________      ,________,________,________,________,________, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT,________,________,________            ,________,________,________,         \
 	//├──────────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───────────┬────────┼────────┼────────┼────────┤────────┤
-	//│                  │        │        │        │        │        │        │        │        │        │        │                │        │        │        │        │ Action │
-	   ________          ,________,________,________,________,________,________,________,________,________,________,________        ,________,________, M_WB   ,________, M_AC   ,\
-	//├─────────┬────────┴┬───────┴─┬──────┴────────┴────────┴────────┴────────┴────────┴────────┼────────┴───┬────┴───────┬────────┼────────┼────────┼────────┼────────┤ Centre │
-	//│ FN → 2  │         │         │ F22    	                                                    │            │        │        │        │        │        │        │
-	    MO(2)   ,________ ,________ , KC_F22                                                     ,________    ,________    , M_LEFT ,________, M_RGHT ,________,________
+	//│                  │        │        │        │        │        │        │        │        │        │        │                │        │        │        │        │        │
+	   ________          ,________,________,________,________,________,________,________,________,________,________,________        ,________,________,________,________,________,\
+	//├─────────┬────────┴┬───────┴─┬──────┴────────┴────────┴────────┴────────┴────────┴────────┼────────┴───┬────┴───────┬────────┼────────┼────────┼────────┼────────┤        │
+	//│ FN → 2  │         │         │ Windows Key                                                │            │            │        │        │        │        │        │        │
+	    MO(2)   ,________ ,________ , KC_LGUI                                                    ,________    ,________    , M_LEFT ,________, M_RGHT ,________,________
 	//└─────────┴─────────┴─────────┴────────────────────────────────────────────────────────────┴────────────┴────────────┴────────┴────────┴────────┴────────┴────────┴────────┘
 	),
 	
